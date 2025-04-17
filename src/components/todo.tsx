95996e4afc11efa6e0201f06c6b08003f4f88932
calmdown1987 <calmdown1987@icloud.com>
2025-04-18 05:47:02 +0900

    Finish feature/complated_delete_flg(ReactでTodoアプリを作ろう_3完了)

---
+++ src/components/todo.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+
+type Todo = {
+  title: string;
+  readonly id: number;
+  completed_flg: boolean;
+  delete_flg: boolean,
+};
+
+const Todos: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [text, setText] = useState('');
+  const [nextId, setNextId] = useState(1);
+
+  const handleSubmit = () => {
+    if(!text) return;
+
+    const newTodo: Todo = {
+      title: text,
+      id: nextId,
+      completed_flg: false,
+      delete_flg: false
+    };
+
+    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+    setNextId(nextId + 1);
+
+    setText('');
+  };
+
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, title: value };
+        }
+        return todo;
+      });
+
+      return newTodos;
+    });
+  };
+
+  const handleCheck = (id: number, completed_flg: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed_flg };
+        }
+        return todo;
+      });
+  
+      return newTodos;
+    });
+  };
+
+  const handleRemove = (id: number, delete_flg: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, delete_flg };
+        }
+        return todo;
+      });
+  
+      return newTodos;
+    });
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+         <button className="insert-btn" type="submit">追加</button>
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed_flg}
+                onChange={() => handleCheck(todo.id,!todo.completed_flg)}
+              />
+              <input
+                type="text"
+                value={todo.title}
+                disabled={todo.completed_flg}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+              <button onClick={() => handleRemove(todo.id, !todo.delete_flg)}>
+                {todo.delete_flg ? '復元' : '削除'}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
