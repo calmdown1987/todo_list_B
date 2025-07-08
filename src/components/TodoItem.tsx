20bf9f1942c1478fe443cff4e640c8a6ff76001b
calmdown1987 <calmdown1987@icloud.com>
2025-07-08 15:38:20 +0900

    TodoItem.tsxの半角スペース・空行の修正

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -31,41 +31,41 @@ const TodoItem: React.FC<TodoItemProps> = ({
         <TodoSelect
           progress={todo.progress}
           onChangeProgress={(newProgress) =>
-          onChangeTodoField(todo.id, "progress", newProgress)
+            onChangeTodoField(todo.id, "progress", newProgress)
           }
         />
         {/* 開始日・完了予定日ピッカー */}
         <DateRangePicker
           start_date={todo.start_date}
           complete_date={todo.complete_date}
-
           onChangeStartDate={(startDate) =>
-          onChangeTodoField(todo.id, "start_date", startDate)}
-
+            onChangeTodoField(todo.id, "start_date", startDate)
+          }
           onChangeCompleteDate={(completeDate) =>
-          onChangeTodoField(todo.id,"complete_date", completeDate)}
+            onChangeTodoField(todo.id, "complete_date", completeDate)
+          }
         />
-        {/* タイトル入力 */}
+        {/* タイトル編集フォーム */}
         <TitleEditorField
           title={todo.title}
           progress={todo.progress}  
           onEditTitle={(newTitle) =>
-          onChangeTodoField(todo.id,"title",newTitle)  
+            onChangeTodoField(todo.id, "title", newTitle)
           }
         />
-
-        {/* 編集ボタン（アコーディオン開閉） */}
+        {/* 編集ボタン（アコーディオン開閉）*/}
         <EditorButton
           todoId={todo.id}
           isOpen={isOpen}
           onToggleOpen={onToggleOpen}
         />
-
         {/* 削除　復元ボタン */}
         <DeleteButton
           isDeleted={todo.delete_flg}
           isCompleted={todo.completed_flg}
-          onToggleDeleted={() =>onChangeTodoField(todo.id,"delete_flg",!todo.delete_flg)}
+          onToggleDeleted={() => 
+            onChangeTodoField(todo.id, "delete_flg", !todo.delete_flg)
+          }
         />
       </li>
 
@@ -73,6 +73,7 @@ const TodoItem: React.FC<TodoItemProps> = ({
       <AccordionPanel
         isOpen={isOpen}
       />
+
     </>
   );
 };
