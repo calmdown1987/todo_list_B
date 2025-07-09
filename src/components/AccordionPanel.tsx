4e68ca1b7e281aad757fddb18fc38a4d9c297a7e
calmdown1987 <calmdown1987@icloud.com>
2025-07-09 20:15:18 +0900

    AccordionPanel.tsxの半角スペースを修正

--- src/components/AccordionPanel.tsx
+++ src/components/AccordionPanel.tsx
@@ -4,18 +4,18 @@ interface AccordionPanelProps {
   isOpen: boolean;
 }
 
-const AccordionPanel :React.FC<AccordionPanelProps> = ({
-  isOpen
+const AccordionPanel: React.FC<AccordionPanelProps> = ({
+  isOpen,
 }) => {
   if (!isOpen) return null;
-  return isOpen && (
+  return (
     <li className="todo-detail-wrapper">
-    <textarea
-      className="todo-detail"
-      placeholder="## 進捗状況&#10;## 内容&#10;## 背景&#10;## 改善内容"
-    />
+      <textarea
+        className="todo-detail"
+        placeholder="## 進捗状況&#10;## 内容&#10;## 背景&#10;## 改善内容"
+      />
     </li> 
-  )
-}
+  );
+};
 
 export default AccordionPanel;
